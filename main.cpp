62857f8b2e90c0ed86de61ddaaabba2a035ebeda
Dom Silva <90434787+dsilva743@users.noreply.github.com>
2022-11-21 08:53:55 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "Splaytree.h"
+
+int main() {
+    
+    //Ignore lines 7 through 12. For testing purposes.
+    Splaytree test;
+
+    test.insert((2));
+    test.insert((1));
+    test.insert((3));
+    test.search(4);
+
+    return 0;
+}
