b2e42a405335fa0fe078d03fa54427adcfe4f5e5
Dom Silva <90434787+dsilva743@users.noreply.github.com>
2022-11-22 21:12:47 -0500

    Update Splaytree.h

--- Splaytree.h
+++ Splaytree.h
@@ -7,11 +7,33 @@ class Node{
         int data;
         Node* left;
         Node* right;
+        Node* parent;
+        bool isLeftChild(){
+            if(parent == nullptr){
+                return false;
+            }
+            else{
+                return parent -> left == this;
+            }
+        }
+        bool isRightChild(){
+            if(parent == nullptr){
+                return false;
+            }
+            else{
+                return parent -> right == this;
+            }
+        }
 
-        Node(int d){
+        bool isRoot(){
+            return parent == nullptr;
+        }
+
+        Node(int d, Node* p){
             data = d;
             left = nullptr;
             right = nullptr;
+            parent = p;
         }
 
 };
@@ -21,6 +43,13 @@ class Splaytree {
 
     private:
         Node* root = nullptr;
+        void bringToTop(Node* x);
+        void zig(Node* x, Node* p, Node* g);
+        void zigZig(Node* x, Node* p, Node* g);
+        void zigZag(Node* x, Node* p, Node* g);
+        void zag(Node* x, Node* p, Node* g);
+        void zagZag(Node* x, Node* p, Node* g);
+        void zagZig(Node* x, Node* p, Node* g);
 
     public:
         Splaytree();
