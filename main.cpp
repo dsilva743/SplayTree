abe585b178c6714b16236646a4531b1a59a549c3
Dom Silva <90434787+dsilva743@users.noreply.github.com>
2022-11-27 18:26:42 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,13 +3,32 @@
 
 int main() {
     
-    //Ignore lines 7 through 12. For testing purposes.
+    //Testing 
     Splaytree test;
 
-    test.insert((2));
-    test.insert((1));
-    test.insert((3));
-    test.search(4);
+    test.insert(10);
+    test.insert(7);
+    test.insert(15);
+    test.insert(1);
+    test.insert(9);
+    test.insert(13);
+    test.insert(20);
+    test.insert(-1);
+    test.insert(30);
+
+    std::cout << "Tree created level order " << std::endl;
+    test.levelOrder();
+    test.search(15);
+    std::cout << "After searching for 15 " << std::endl;
+    test.levelOrder();
+
+//    test.insert((5));
+//    test.insert((3));
+//    test.insert((4));
+//    test.insert((6));
+//    test.insert((1));
+//
+//    test.search(3);
 
     return 0;
 }
