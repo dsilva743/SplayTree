3411dcc9596210eff333074254f3b38b2541dae1
Dom Silva <90434787+dsilva743@users.noreply.github.com>
2022-12-04 20:27:57 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,34 +1,75 @@
 #include <iostream>
 #include "Splaytree.h"
 
+int menu(){
+    //prints the menu and gets the user input for menu options and returns it
+    //will only return valid options
+    int choice = 0;
+
+    while(choice < 1 || choice > 4){
+        std::cout << "1. Insert Number" << std::endl;
+        std::cout << "2. Search for number" << std::endl;
+        std::cout << "3. Print level order" << std::endl;
+        std::cout << "4. Quit" << std::endl;
+
+        std::cin >> choice;
+    }
+    return choice;
+}
+
 int main() {
-    
-    //Testing 
-    Splaytree test;
 
-    test.insert(10);
-    test.insert(7);
-    test.insert(15);
-    test.insert(1);
-    test.insert(9);
-    test.insert(13);
-    test.insert(20);
-    test.insert(-1);
-    test.insert(30);
+    Splaytree tree;
+    int choice = 0;
 
-    std::cout << "Tree created level order " << std::endl;
-    test.levelOrder();
-    test.search(15);
-    std::cout << "After searching for 15 " << std::endl;
-    test.levelOrder();
+    while(choice != 4){
+        choice = menu();
 
-//    test.insert((5));
-//    test.insert((3));
-//    test.insert((4));
-//    test.insert((6));
-//    test.insert((1));
+        if(choice == 1){
+            //insert number
+            std::cout << "Enter a number: ";
+            int num;
+            std::cin >> num;
+            tree.insert(num);
+        }
+        else if( choice == 2){
+            //search number
+            std::cout << "Enter a number to search for: ";
+            int num;
+            std::cin >> num;
+            bool results = tree.search(num);
+            std::cout << "Result: " << results << std::endl;
+
+
+        }
+        else if(choice == 3){
+            //tree level order
+            std::cout << "------ Level Order ------"<< std::endl;
+            tree.levelOrder();
+            std::cout << "-------------------------" << std::endl;
+        }
+        else{
+            std::cout << "Goodbye" << std::endl;
+        }
+    }
+
+//    test.insert(10);
+//    test.insert(7);
+//    test.insert(15);
+//    test.insert(1);
+//    test.insert(9);
+//    test.insert(13);
+//    test.insert(20);
+//    test.insert(-1);
+//    test.insert(30);
 //
-//    test.search(3);
+//    std::cout << "Tree created level order " << std::endl;
+//    test.levelOrder();
+//    test.search(15);
+//    std::cout << "After searching for 15" << std::endl;
+//    test.levelOrder();
+
+
 
     return 0;
 }
