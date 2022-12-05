aa34cb143dfffd1034c81ba011ff3be3af61577d
Dom Silva <90434787+dsilva743@users.noreply.github.com>
2022-12-05 15:00:46 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -53,23 +53,6 @@ int main() {
         }
     }
 
-//    test.insert(10);
-//    test.insert(7);
-//    test.insert(15);
-//    test.insert(1);
-//    test.insert(9);
-//    test.insert(13);
-//    test.insert(20);
-//    test.insert(-1);
-//    test.insert(30);
-//
-//    std::cout << "Tree created level order " << std::endl;
-//    test.levelOrder();
-//    test.search(15);
-//    std::cout << "After searching for 15" << std::endl;
-//    test.levelOrder();
-
-
 
     return 0;
 }
